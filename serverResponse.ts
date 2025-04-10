d076deabe2b0be27f7029187222d3190f330d85b
Daniil Bobrov <allodrmen@yandex.ru>
2025-04-10 21:07:35 +0300

    serverResponse 4.8

---
+++ serverResponse.ts
@@ -0,0 +1,46 @@
+enum Status {
+    SUCCESS = 'success',
+    ERROR = 'error'
+}
+
+interface IPayment {
+    sum: number,
+    from: number,
+    to: number
+}
+
+interface IRequestBody extends IPayment {}
+
+interface IdataSuccess extends IPayment {
+    databaseId: number
+}
+
+interface IdataError {
+    errorMessage: string
+    errorCode: number
+}
+
+// не лучший вариант
+// interface IRequestResponse {
+//     status: Status
+//     data: IdataSuccess | IdataError
+// }
+
+interface IRequestResponseSuccess {
+    status: Status.SUCCESS
+    data: IdataSuccess
+}
+
+interface IRequestResponseError {
+    status: Status.ERROR
+    data: IdataError
+}
+
+async function makeRequest (body: IRequestBody): Promise<IRequestResponseSuccess | IRequestResponseError> {
+    const response = await fetch('/url', {
+        method: 'POST',
+        body: JSON.stringify(body)
+    })
+    const data = response.json()
+    return data
+}
