969169cc001f31dcb9313d964fa74bb18241e009
Daniil Bobrov <allodrmen@yandex.ru>
2025-04-22 20:25:00 +0300

    Add generics.ts file to define a Record type for car properties

---
+++ generics.ts
@@ -0,0 +1,10 @@
+// тип рекорд создан для того чтобы можно было создать объект с любыми свойствами
+
+const car: Record<string, string | number>  = {
+  name: 'Toyota',
+  year: 2020,
+  color: 'red',
+}
+
+
+
